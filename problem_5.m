8bd111593a9f234178eea03b8549c1540cf20ddb
acolin02 <32846947+acolin02@users.noreply.github.com>
2019-12-09 23:32:36 -0500

    Update problem_5.m

--- problem_5.m
+++ problem_5.m
@@ -11,8 +11,10 @@ clc
 load('x4fun.mat')
 
 N_SAMP = 1024;
+N = 11;
 
-w = linspace(0, 2 * pi * (1 - 1 / N_SAMP), N_SAMP); 
+w = linspace(0, 2 * pi * (1 - 1 / N_SAMP), N_SAMP);
+w = transpose(w);
 
 h_n = zeros(N_SAMP, 1);
 h_n(1 : 11) = 1;
@@ -25,7 +27,7 @@ Y_jw = fft(y_n, N_SAMP);
 H_jw = fft(h_n, N_SAMP);
 
 % Replace H_jw_inv with Wiener filter!
-eigen = linspace(0, 1000, 20); 
+eigen = linspace(0, 1000, 6); 
 noise_n = 0.01 * randn(N_SAMP, 1);
 noise_jw = fft(noise_n);
 Y_jw_noise = Y_jw + noise_jw;
